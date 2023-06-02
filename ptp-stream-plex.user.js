a9b3ffde536cce9ca9b21cfcbe13ff2c14430129
cc-coding3 <pla4477@yandex.ru>
2023-06-02 03:35:17 +0300

    test

--- ptp-stream-plex.user.js
+++ ptp-stream-plex.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PTP - Stream Plex
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @match        https://passthepopcorn.me/*
 // @grant        none
 // @updateURL    https://github.com/cc-coding3/ptp-stream-plex/raw/master/ptp-stream-plex.user.js
@@ -11,6 +11,6 @@
 
     'use strict';
     console.log("This userscript is hosted in github. 0.1")
-    console.log('update v6');
+    console.log('update v7');
 
 })();
