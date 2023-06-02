5258a14320530bda9a6015b173e7b35eb4451ffd
cc-coding3 <pla4477@yandex.ru>
2023-06-02 04:14:52 +0300

    update

--- ptp-stream-plex.user.js
+++ ptp-stream-plex.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PTP - Stream Plex
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @match        https://passthepopcorn.me/*
 // @import       /test-import.js
 // @grant        none
